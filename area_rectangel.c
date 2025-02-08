3bfae6d63f7ab5734df40e6f89cef4aa0f132df6
samanta1401 <psamanta1401@gmail.com>
2025-02-08 19:12:51 +0530

    area of rectangle added

--- area_rectangel.c
+++ area_rectangel.c
@@ -1,21 +1,25 @@
 #include<stdio.h>
 
-float len, bre;
-float peri, are;
-void main(){
-    
+// Function prototype
+void periarea(float length, float breadth, float *peri, float *are); //pointer store address of peri and are
 
-    int periarea(float length,float breadth,float *peri, float *are);
-    printf("Enter the length: ");
-    scanf("%f%f",len,bre);
-    periarea(len,bre,&len,&are);
-    printf("\nPerimeter of the rectangle is %f metres", peri); 
-    printf("\nArea of the rectangle is %f sq. metres", are);
+void main(){
+    float len, bre;
+    float peri, are;
+    printf("Enter the length and breadth: ");
+    scanf("%f %f", &len, &bre); //store input val to address // enter 2 input with space
+    //function call
+    periarea(len,bre,&peri,&are); // passing val of len and bre and address of peri and are to the pointer
+
+    printf("\nPerimeter of the rectangle is %f metres", peri); // print val or peri
+    printf("\nArea of the rectangle is %f sq. metres", are); // print val or are
 
     
 }
-int periarea(float length, float breadth, float *peri,float *are){
-        *peri = 2*(len+bre);
-        *are = len*bre;
-        return *peri, *are;
+// Function definition
+void periarea(float length, float breadth, float *peri,float *are){
+    
+        *peri = 2*(length + breadth); //store val of peri to address of peri 
+        *are = length*breadth; //store val of are to address of are
+        
     }
