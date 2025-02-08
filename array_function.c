a13d8e57b6271035ce0746ea16447303295fadcd
samanta1401 <psamanta1401@gmail.com>
2025-02-08 20:55:16 +0530

    arr pointer added

--- array_function.c
+++ array_function.c
@@ -19,6 +19,25 @@ void printArray(int *ptr, int n){
 // }
 int main(){
     int arr[] = {2,3,4,67,23,4,34,5};
+    printf("%u \n", arr); // array pointer always points to the beginning of the array i.e. address of the first element
+    printf("%u \n", &arr); // print address of the first element
+    printf("%u \n", &arr + 1); // print address of the second element
+    printf("%d \n", arr[0]); // print first element
+    printf("%u \n", &arr[0]); // print address of the first element
+    printf("%u \n", &arr[7]); // print address of the last element
+    printf("%d \n", arr[7]); // print  last element
+
+    int *ptr = arr; // store first element address to ptr pointer
+    int *ptr2;
+    ptr2 = &arr[7]; // store last element address to ptr2 pointer
+
+    printf("%u \n", ptr);        // print address store in ptr
+    printf("%u \n", ptr2);       // print address store in ptr2
+    printf("%d \n", *ptr);       // print value of address store in  ptr
+    printf("%d \n", *(ptr + 1)); // print value of address store in next of ptr
+    printf("%d \n", arr[1]);     // print value
+
+
     printArray(arr,8);
     printf("%d",arr[7]);
     
